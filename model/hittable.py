4ee94689403d2a74a88cc46f1368421a553c2bdc
ythere <1069043992@qq.com>
2022-11-11 20:41:58 +0800

    add hittable list

--- model/hittable.py
+++ model/hittable.py
@@ -27,3 +27,26 @@ class hittable:
     @abstractmethod
     def hit(self, r: Ray, t_min: float, t_max: float, rec: hit_record) -> bool:
         pass
+
+
+class hittable_list(hittable):
+    def __init__(self, objects: list[hittable]) -> None:
+        self.objects = objects
+
+    def hit(self, r: Ray, t_min: float, t_max: float, rec: hit_record) -> bool:
+        temp_rec = hit_record()
+        hit_anything = False
+        closed_so_far = t_max
+
+        for obj in self.objects:
+            if obj.hit(r, t_min, closed_so_far, temp_rec):
+                hit_anything = True
+                closed_so_far = temp_rec.t
+                rec = temp_rec
+        return hit_anything
+
+    def clear(self):
+        self.objects.clear()
+
+    def add(self, obj: hittable):
+        self.objects.append(obj)
